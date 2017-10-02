5f12b89a6cce82d8aef1c9b279fa80b280bc66c8
Kalvin Chang <kalvin1204@gmail.com>
2017-10-01 20:07:04 -0700

    made color bar collapsible. fix positioning of colorBar later

--- sketch.js
+++ sketch.js
@@ -1,4 +1,26 @@
-//source: https://socketio-whiteboard.now.sh/
+/*adapted from: https://socketio-whiteboard.now.sh/
+Copyright (c) 2014-2017 Automattic <dev@cloudup.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 'use strict';
 
 (function() {
@@ -9,6 +31,7 @@
   var colors = document.getElementsByClassName('color');
   var thick = document.getElementById('thickness-slider');
   var papers = document.getElementsByClassName('papers');
+  var currentColor = document.getElementsByClassName('currentColor')[0];
 
   var context = canvas.getContext('2d');
   var undo = document.getElementById('undo');
@@ -83,6 +106,25 @@
 
   //switch paper
   var papers = ['plain', 'assets/graph.png', 'assets/lined.jpg'];
+  //make colors collapsible
+  var colorBar = document.getElementById('colorBar');
+  //clicking on current-color -> display the colors horizontally (unhide)
+  var barDisplay = true;
+  currentColor.onclick = function() {
+    barDisplay = !barDisplay;
+    if (barDisplay) {
+      colorBar.style.display = 'none';
+    } else {
+      colorBar.style.display = 'inline-flex';
+    }
+  }
+  // for (var i = 0; i < color.length; i++) {
+  //  color[i].onclick = function() {
+  //    this.classList.toggle('active');
+  //  }
+  // }
+
+
 
   function drawLine(x0, y0, x1, y1, color, thickness, emit){
     context.beginPath();
@@ -129,6 +171,7 @@
 
   function onColorUpdate(e){
     current.color = e.target.className.split(' ')[1];
+    currentColor.style.background = current.color;
   }
 
   function onThickUpdate(e){
