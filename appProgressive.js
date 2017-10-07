db69a98fe4f8b2b0a2c050a12955cdf0b04a9234
Darien Tsai <smartdragondt@gmail.com>
2017-10-07 09:51:18 -0700

    added oauth authentication

--- appProgressive.js
+++ appProgressive.js
@@ -18,7 +18,7 @@ $("#HomeCheckList ul li img").click(function(){
 var recentUnchecked = 0;
 function checkMarks(){
   for(var i = 0; i <= $('#HomeBody ul li img').length; i ++){
-    if ($('#HomeBody ul li img').eq(i).attr('src') === "assets/unchecked.svg" )  {
+    if ($('#HomeChecklist ul li img').eq(i).attr('src') === "assets/unchecked.svg" )  {
       recentUnchecked = i;
       break;
     }
@@ -51,6 +51,66 @@ request.execute(function (response) {
   console.log(response);
 })
 
+// The client ID is obtained from the {{ Google Cloud Console }}
+// at {{ https://cloud.google.com/console }}.
+// If you run this code from a server other than http://localhost,
+// you need to register your own client ID.
+var OAUTH2_CLIENT_ID = '780139124013-o6mce862so8m6le123l82924c3vgeroc.apps.googleusercontent.com';
+var OAUTH2_SCOPES = [
+  'https://www.googleapis.com/auth/youtube'
+];
+
+// Upon loading, the Google APIs JS client automatically invokes this callback.
+googleApiClientReady = function() {
+  gapi.auth.init(function() {
+    window.setTimeout(checkAuth, 1);
+  });
+}
+
+// Attempt the immediate OAuth 2.0 client flow as soon as the page loads.
+// If the currently logged-in Google Account has previously authorized
+// the client specified as the OAUTH2_CLIENT_ID, then the authorization
+// succeeds with no user intervention. Otherwise, it fails and the
+// user interface that prompts for authorization needs to display.
+function checkAuth() {
+  gapi.auth.authorize({
+    client_id: OAUTH2_CLIENT_ID,
+    scope: OAUTH2_SCOPES,
+    immediate: true
+  }, handleAuthResult);
+}
+
+// Handle the result of a gapi.auth.authorize() call.
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    // Authorization was successful. Hide authorization prompts and show
+    // content that should be visible after authorization succeeds.
+    $('.pre-auth').hide();
+    $('.post-auth').show();
+    loadAPIClientInterfaces();
+  } else {
+    // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
+    // client flow. The current function is called when that flow completes.
+    $('#login-link').click(function() {
+      gapi.auth.authorize({
+        client_id: OAUTH2_CLIENT_ID,
+        scope: OAUTH2_SCOPES,
+        immediate: false
+        }, handleAuthResult);
+    });
+  }
+}
+
+// Load the client interfaces for the YouTube Analytics and Data APIs, which
+// are required to use the Google APIs JS client. More info is available at
+// https://developers.google.com/api-client-library/javascript/dev/dev_jscript#loading-the-client-library-and-the-api
+function loadAPIClientInterfaces() {
+  gapi.client.load('youtube', 'v3', function() {
+    handleAPILoaded();
+  });
+}
+auth.js
+
 //this tests checkMarks method
 console.log($('#HomeBody ul li')[recentUnchecked].innerHTML.substring(32));
 
@@ -67,7 +127,6 @@ function searcher() {
     type: 'video',
     key: 'AIzaSyD7NybHdEUpObHST_6kkWtK3TYVWZnYKV8'},
     function(data){
-      // not done, data & tokens
       var nextToken = data.nextPageToken;
       var prevToken = data.prevPageToken;
 
