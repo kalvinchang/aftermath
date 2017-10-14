292c6b80a97e8518a85cbb7187b6a012a1d43545
Kalvin Chang <kalvin1204@gmail.com>
2017-10-13 21:19:49 -0700

    still trying to fix issues with reorganization

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generatedMessage} = require('../utils/message') //import function, from another directory
+const {generatedMessage} = require('./utils/message'); //import function, from another directory
 
-const publicPath = path.join(__dirname, '../');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
