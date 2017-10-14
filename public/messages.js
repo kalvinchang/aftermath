11868dbfba47b710a227dcb1a3eebbf8b471b37b
Kalvin Chang <kalvin1204@gmail.com>
2017-10-13 21:27:45 -0700

    trying to fix messaging issue where the message is displayed twice

--- public/messages.js
+++ public/messages.js
@@ -13,28 +13,12 @@ socket.on('disconnect', function (){
 socket.on('newMessage', function (message){
   console.log('New message arrived.', message); // prints email and its object data
   var li = jQuery('<li></li>'); //creates html object
-  li.text(`${message.from}: ${message.text}`)  // gets the edata
+  li.text(message.from + ': ' + message.text);  // gets the edata
 
   jQuery('#messages').append(li); //gets the html li data onto the html ordered list
 });
 
 // the object in the function arguments = thing used in the console.log to display the object
-
-jQuery('#message-form').on('submit', function(e){
-  e.preventDefault();// prevent default behavior of button that refreshes page
-
-  socket.emit('createMessage', {
-    from: 'User', //replace w/ actual username
-    text: jQuery('[name=message]').val() // jquery selects any obj with name message, and val() grabs that value
-  }, function(){
-
-  })
-});
-socket.on('createMessage', (message,callback) => { // the thing in the 2nd argument's parentheses is the event
-  console.log('createMessage', message);
-  socket.emit('newMessage', generatedMessage(message.from, message.text))
-  callback('This is from the server');
-});
 
 var messageTextbox = jQuery('[name=message]');
 
