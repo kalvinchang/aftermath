fe408bdf707714c80c574079beabe5f7010fa1bd
GoodLuckdotexe <siueric0010@gmail.com>
2017-10-19 18:32:09 -0700

    test

--- public/messages.js
+++ public/messages.js
@@ -10,41 +10,34 @@ socket.on('disconnect', function (){
   console.log('Connected dropped/ Disconnected from Server');
 });
 
+/*The original stuff
 socket.on('newMessage', function (message){
   console.log('New message arrived.', message); // prints email and its object data
   var li = jQuery('<li></li>'); //creates html object
-  li.text(`${message.from}: ${message.text}`)  // gets the edata
+  li.text(message.from + ': ' + message.text);  // gets the edata
+  jQuery('#messages').append(li); //gets the html li data onto the html ordered list
+});
+*/
+
+socket.on('newMessage', function (message){
+  console.log('New message arrived.', message); // prints email and its object data
+  var li = jQuery('<li><div class="receive"><div class="sender"><img src="assets/DefaultProfile.svg" alt="'+message.from+'"><h3>' +message.from+ '</h3></div><p>'+message.text+'</p></div></li>'); //creates html object
 
   jQuery('#messages').append(li); //gets the html li data onto the html ordered list
 });
 
 // the object in the function arguments = thing used in the console.log to display the object
 
-jQuery('#message-form').on('submit', function(e){
-  e.preventDefault();// prevent default behavior of button that refreshes page
-
-  socket.emit('createMessage', {
-    from: 'User', //replace w/ actual username
-    text: jQuery('[name=message]').val() // jquery selects any obj with name message, and val() grabs that value
-  }, function(){
-
-  })
-});
-socket.on('createMessage', (message,callback) => { // the thing in the 2nd argument's parentheses is the event
-  console.log('createMessage', message);
-  socket.emit('newMessage', generatedMessage(message.from, message.text));
-  callback('This is from the server');
-});
-
 var messageTextbox = jQuery('[name=message]');
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: messageTextbox.val()
-  }, function() {
-    messageTextbox.val('');
-  });
-});
+  if (messageTextbox.val() != '') {
+    socket.emit('createMessage', {
+      from: 'User', //replace w/ actual user later
+      text: messageTextbox.val()
+    }, function() {
+      messageTextbox.val('');
+    });
+  }
